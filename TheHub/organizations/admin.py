4a0f1b905792b7814e5545c04bec2f4238be4c86
Cezekwe1 <eaezekwem@gmail.com>
2019-04-30 15:26:32 -0400

    make migrations

--- TheHub/organizations/admin.py
+++ TheHub/organizations/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Organization, Member
 # Register your models here.
+
+admin.site.register(Organization)
+admin.site.register(Member)
--- TheHub/organizations/models.py
+++ TheHub/organizations/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Organization(models.Model):
     name = models.CharField(max_length=100)
 
 
-
-class Member(model.Model):
-    user 
-
+class Member(models.Model):
+    users = models.ManyToManyField(User)
+    organizations = models.ManyToManyField(Organization)
