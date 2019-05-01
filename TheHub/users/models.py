a815cbff2123ba9a85f28ad55111976497056731
Cezekwe1 <eaezekwem@gmail.com>
2019-05-01 15:48:04 -0400

    create friends

--- TheHub/users/models.py
+++ TheHub/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from organizations.models import Membership, Organization
 # Create your models here.
+    
+class Friends(models.Model):
+    inviter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends.inviter')
+    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends.target')
+
+class Invite(models.Model):
+    inviter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='invite.inviter') 
+    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name='invite.target')
+    accepted = models.NullBooleanField(default=null)
+
