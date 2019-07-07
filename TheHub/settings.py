258a419cebb92ca3be4689562f65f786329db8d8
Cezekwe1 <eaezekwem@gmail.com>
2019-07-07 18:46:46 -0400

    fix cors

--- TheHub/settings.py
+++ TheHub/settings.py
@@ -136,7 +136,25 @@ USE_TZ = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = False
 CORS_ORIGIN_WHITELIST = ['https://the-hub-client.herokuapp.com','localhost:3000']
-
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
