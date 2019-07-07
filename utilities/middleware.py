9e390a45fd46a3a748f68c45490ba7bb26f6c63f
Cezekwe1 <eaezekwem@gmail.com>
2019-07-07 16:52:22 -0400

    remove admin panel

--- utilities/middleware.py
+++ utilities/middleware.py
@@ -5,7 +5,6 @@ class APIMiddleWare:
         self.get_response = get_response
     
     def __call__(self,request):
-        print(request.META)
         if  'HTTP_REFERER' in request.META:
             if request.META['HTTP_REFERER'] == 'https://the-hub-client.herokuapp.com':
                 response = self.get_response(request)
